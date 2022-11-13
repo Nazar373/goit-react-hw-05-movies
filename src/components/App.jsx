b0144d6e517e72683c9e148e3fc719e0e1371861
Nazar Lyubuska <nazar.lyubuska@gmail.com>
2022-11-13 11:28:41 +0200

    m

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,23 @@
 import React, { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from '../components/Layout /Layout';
+
 const Home = lazy(() => import('../pages /Home /Home'));
-const Movies = lazy(() => import('../pages /Movies /Movies'))
+const Movies = lazy(() => import('../pages /Movies /Movies'));
 const MovieDetails = lazy(() => import('../pages /MovieDetails/MovieDetails'));
-const Cast = lazy(() => import('../pages /Cast/Cast'))
-const Reviews = lazy(() => import('../pages /Reviews/Reviews'))
+const Cast = lazy(() => import('../pages /Cast/Cast'));
+const Reviews = lazy(() => import('../pages /Reviews/Reviews'));
 
 export const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />}/>
-          <Route path="movies" element={<Movies />}/>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
           <Route path="/movies/:movieId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />}/>
-            <Route path="reviews" element={<Reviews />}/>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
       </Routes>
